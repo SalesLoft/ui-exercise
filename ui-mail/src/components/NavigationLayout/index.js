3943aca6ef53b861df6052557516d2ffd40065c0
Adrianna Valdivia <avaldivi@ggc.edu>
2018-12-01 20:47:43 -0500

    Help somewhat with with mobile view but we're too far gone.

--- ui-mail/src/components/NavigationLayout/index.js
+++ ui-mail/src/components/NavigationLayout/index.js
@@ -1,19 +1,34 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { NavLink } from 'react-router-dom'
 import PropTypes from 'prop-types';
 import './style.scss'
 
-const NavigationLayout = ({ tags, children }) => {
-  let tagsUsed = tags.messages.map(message => message.tags)
-  let uniqueTags = [...new Set(tagsUsed.flat())];
+export default class NavigationLayout extends Component {
+  constructor(props) {
+    super(props)
 
-  return (
-    <div className="layout">
+    this.state = {
+      openMenu: false
+    }
+  }
+
+  openCloseMenu(openMenu) {
+    this.setState({ openMenu: !this.state.openMenu})
+  }
+
+  render() {
+    const tagsUsed = this.props.tags.messages.map(message => message.tags)
+    const uniqueTags = [...new Set(tagsUsed.flat())];
+    const {openMenu} = this.state
+
+    return (
+      <div className="layout">
       <div className="top-navigation">
-        Ui-MAiL
+        <div className="logo">Ui-MAiL</div>
+        <b onClick={this.openCloseMenu.bind(this)}>open</b>
       </div>
       <div className="panel-layout">
-        <div className="sidebar">
+        <div className={openMenu ? 'sidebar sidebar-open': 'sidebar sidebar-closed'}>
           <NavLink className="sidebar-item" activeClassName="active" to="/">Inbox</NavLink>
           <b className="tag-title"> Tags Available ({uniqueTags.length})</b>
             {
@@ -29,15 +44,16 @@ const NavigationLayout = ({ tags, children }) => {
           <NavLink className="sidebar-item" activeClassName="active" to="/messages/archived">Archived</NavLink>
         </div>
         <div className="messages-wrapper">
-          {children}
+          {this.props.children}
         </div>
       </div>
     </div>
-  );
-};
+    )
+  }
+}
+
 
 NavigationLayout.propTypes = {
   children: PropTypes.any
 };
 
-export default NavigationLayout
